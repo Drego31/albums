4effb45e639bb78b64d47ee024a28653725f40cf
Drego31 <dominik.betka@arbuz.team>
2019-05-31 00:15:18 +0200

    correcting photos appearance

--- src/components/molecules/MiniPhoto.js
+++ src/components/molecules/MiniPhoto.js
@@ -2,13 +2,9 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const MMiniPhoto = (props) => {
-  function getPhotoUrl(url) {
-    return typeof url !== 'undefined' ? {backgroundImage: `url(${url})`} : {}
-  }
-
   return (
     <Link to={`/photo/${props.photo.id}`} className="m-mini-photo">
-      <div className="a-photo" style={getPhotoUrl(props.photo.url)} />
+      <img className="a-photo f-mini" src={props.photo.url} />
       <div className="a-title f-photo">{props.photo.title}</div>
     </Link>
   )
